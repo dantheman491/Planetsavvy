3963cf9f4ac1cc1f7a36a2633242b8775e2b1012
Daniel Ramirez <danielramirez@daniels-mbp.home>
2020-12-21 16:19:59 -0500

    update v1.1

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-const DOMAIN = 'https://api.nasa.gov//';
+
+const DOMAIN = 'https://api.nasa.gov/planetary/apod/?api_key=';
 const API_KEY = 'NzCwqiVcoXazjMjrCdZ1bGnACqnypTMrkuVBmdU2' /* Your API key goes here */
-const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`
+const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&date=`
 
 // let searchButton = document.querySelector("#search")
+// button.addEventListener('click', async () => {
+//   let response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=NzCwqiVcoXazjMjrCdZ1bGnACqnypTMrkuVBmdU2&date=${2020 - 10 - 01}`);
+//   console.log(response);
+// });
 
 const getData = async () => {
   try {
     const getDate = document.querySelector('input').value
     console.log(getDate)
     console.log(getDate)
-    const response = await axios.get(`${BASE_URL}s=${getDate}`)
-    console.log(response.data)
-    renderList(response.data)
-    return response
+    let response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=NzCwqiVcoXazjMjrCdZ1bGnACqnypTMrkuVBmdU2&date=${getDate}`);
+    console.log(response)
+    // renderList(response.data)
+    // return response
   } catch (error) {
     console.log()
   }
 }
+
 const searchButton = document.querySelector('#search')
 searchButton.addEventListener('click', getData)
 
-async function renderList(date) {
+function renderList(date) {
   date.forEach((moon) => {
     let moonTitle = document.createElement('h1')
     moonTitle.innerText = moon.Title
@@ -28,18 +34,4 @@ async function renderList(date) {
   })
 }
 
-// adding an event listner to the button that runs the function  when clicked
-// searchButton.addEventListener("click", () => {
-//   console.log("button pressed")
-//   getData()
-// })
-// //an async function to request data from the API
-// //async function getApiRequest() {
-// let getData = async () => {
-//   let API_KEY = 'NzCwqiVcoXazjMjrCdZ1bGnACqnypTMrkuVBmdU2' /* Your API key goes here */
-//   let response = axios(`https://api.nasa.gov/planetary/apod/?api_key=${API_KEY}`);
-//   console.log(response)
-//   let data = response(
-//     console.log(data)
-//   )
-// }
+
