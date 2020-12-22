1fd5e95ecdcdfb3483170a9d04419523a25c35df
Daniel Ramirez <danielramirez@daniels-mbp.home>
2020-12-22 11:15:23 -0500

    update v1.3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const DOMAIN = 'https://api.nasa.gov/planetary/apod/?api_key=';
 const API_KEY = 'NzCwqiVcoXazjMjrCdZ1bGnACqnypTMrkuVBmdU2' /* Your API key goes here */
 const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&date=`
-
+const contentInfo = document.querySelector('.contentGoesHere')
 // let searchButton = document.querySelector("#search")
 // button.addEventListener('click', async () => {
 //   let response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=NzCwqiVcoXazjMjrCdZ1bGnACqnypTMrkuVBmdU2&date=${2020 - 10 - 01}`);
@@ -26,17 +26,38 @@ const searchButton = document.querySelector('#search')
 searchButton.addEventListener('click', getData)
 
 function renderList(moonData) {
-  console.log(moonData)
-  let moonDate = document.createElement('h2')
-  console.log(moonDate)
-  moonDate.innerText = moonData.date
-  console.log(moonDate)
+  contentInfo.innerHTML = ' '
 
-  let spaceImage = document.createElement('img')
-  spaceImage.setAttribute('src', moonData.hdurl)
+  let spaceDiv = document.createElement('div')
+  spaceDiv.innerHTML = `
+    <h2>${moonData.date}</h2>
+    <img src=${moonData.hdurl} />
+    <h3>${moonData.explanation}</h3>
+  `
 
-  document.querySelector('.contentGoesHere').append(spaceImage)
-  document.querySelector('.contentGoesHere').append(moonDate)
+  contentInfo.append(spaceDiv)
+  //refactor for later
+
+
+  //   let moonDate = document.createElement('h2')
+  //   console.log(moonDate)
+  //   moonDate.innerText = moonData.date
+  //   console.log(moonDate)
+
+  //   let spaceImage = document.createElement('img')
+  //   spaceImage.setAttribute('src', moonData.hdurl)
+
+  //   document.querySelector('.contentGoesHere').append(spaceImage)
+  //   document.querySelector('.contentGoesHere').append(moonDate)
+  //   // document.querySelector('.contentGoesHere').remove(moonDate, spaceImage)
+
+  //   let spaceExplanation = document.createElement('h2')
+  //   console.log(spaceExplanation)
+  //   spaceExplanation.innerText = moonData.explanation
+  //   console.log(spaceExplanation)
+  //   document.querySelector('h2').append(spaceExplanation)
+  //   document.querySelector('h2').removeElement(spaceExplanation)
+
 }
 
 
